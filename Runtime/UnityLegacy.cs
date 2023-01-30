c4793d5055c673ba40fa643577c78ddf6b35147d
HollowYr <yar.krishtal@gmail.com>
2023-01-31 01:16:03 +0200

    Math legacy added

--- Runtime/UnityLegacy.cs
+++ Runtime/UnityLegacy.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 
 public static class UnityLegacy
 {
+
+    public static Transform[] GetAllChildrenArray(this Transform transform)
+    {
+        int childCount = transform.childCount;
+        Transform[] result = new Transform[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            result[i] = transform.GetChild(i);
+        }
+        return result;
+    }
+    public static List<Transform> GetAllChildrenList(this Transform transform)
+    {
+        List<Transform> result = new List<Transform>();
+        foreach (Transform child in transform)
+        {
+            result.Add(child);
+        }
+        return result;
+    }
+
     public static float InputHorizontal() => Input.GetAxis("Horizontal");
     public static float InputVertical() => Input.GetAxis("Vertical");
     public static bool InputJump() => Input.GetKeyDown(KeyCode.Space);
