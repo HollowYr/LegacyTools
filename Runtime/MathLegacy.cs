aa82673598d0aea9fedb3714feb71e3ebd651ce3
HollowYr <yar.krishtal@gmail.com>
2023-02-07 13:15:45 +0200

    Remap Upd

--- Runtime/MathLegacy.cs
+++ Runtime/MathLegacy.cs
@@ -7,6 +7,7 @@ public static class MathLegacy
 {
     public static float Remap(this float value, float from1, float to1, float from2, float to2)
     {
+        value = Mathf.Clamp(value, from1, to1);
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 
